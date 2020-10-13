e56e5b787ad112a437571a31437fa27d17d360c4
MCA <melihcanaydin@gmail.com>
2020-10-13 22:51:45 +0300

    SolveMeFirst

---
+++ HackerRank/Algorithms/Warmup/SolveMeFirst/SolveMeFirst.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HackerRank.SolveMeFirst
+{
+    class SolveMeFirst
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Please insert 2 number :");
+            var num1 = int.Parse(Console.ReadLine());
+            var num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(num1 + num2);
+        }
+    }
+}
